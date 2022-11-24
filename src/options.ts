37b8d29c7034c9a8d2a72c390f255d895e070907
Filip Mösner <filip.mosner@seznam.cz>
2022-11-24 15:17:53 +0100

    Rewrite: - color scheme by system, light, dark and custom - apply new settings on live - some improvements

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,28 @@
-function getInputValue(elementId: string): string {
-  return (document.getElementById(elementId) as HTMLInputElement).value;
+function getInputValue(elementId: string): string | boolean {
+  const elm = document.getElementById(elementId)! as HTMLInputElement;
+  if (elm) {
+    if (elm.type === "checkbox") {
+      return elm.checked;
+    } else {
+      if (elm.value === "true") return true;
+      if (elm.value === "false") return false;
+
+      return elm.value;
+    }
+  }
+  throw new Error('Invalid input element');
 }
 
-function setInputValue(elementId: string, value: string): void {
-  (document.getElementById(elementId) as HTMLInputElement).value = value;
+function setInputValue(elementId: string, value: string | number | boolean): void {
+  const elm = document.getElementById(elementId)! as HTMLInputElement;
+  if (elm) {
+    if (elm.type === "checkbox") {
+      elm.checked = !!value;
+    } else {
+      const strVal = typeof value === "string" ? value : value.toString();
+      elm.value = strVal;
+    }
+  }
 }
 
 function setStatus(statusText: string): void {
@@ -14,18 +33,19 @@ function setStatus(statusText: string): void {
 
 function saveOptions(e: Event): void {
   const settings: Settings = {
-    background: getInputValue("background"),
-    foreground: getInputValue("foreground"),
+    background: getInputValue("background") as string,
+    foreground: getInputValue("foreground") as string,
+    colorScheme: getInputValue("colorScheme") as ColorScheme,
     tempUnit: getInputValue("tempUnit") as TempUnit,
-    location: getInputValue("location"),
-    timePattern: getInputValue("timePattern"),
-    datePattern: getInputValue("datePattern"),
+    location: getInputValue("location") as string,
+    timePattern: getInputValue("timePattern") as string,
+    datePattern: getInputValue("datePattern") as string,
     clock: getInputValue("clock") as ClockType,
     font: getInputValue("font") as FontType,
-    leadingZero: getInputValue("zero") === "true",
-    customCss: getInputValue("css"),
-    displayIcon: getInputValue("icon") === "true",
-    activateDebugMode: (document.getElementById("activateDebugMode") as HTMLInputElement).checked
+    leadingZero: getInputValue("zero") as boolean,
+    customCss: getInputValue("css") as string,
+    displayIcon: getInputValue("icon") as boolean,
+    activateDebugMode: getInputValue("activateDebugMode") as boolean,
   };
 
   browser.storage.sync.set(settings)
@@ -43,17 +63,26 @@ function restoreOptions(): void {
     .then(settings => {
       setInputValue("background", settings.background);
       setInputValue("foreground", settings.foreground);
+      setInputValue("colorScheme", settings.colorScheme);
       setInputValue("tempUnit", settings.tempUnit);
       setInputValue("location", settings.location);
       setInputValue("timePattern", settings.timePattern);
       setInputValue("datePattern", settings.datePattern);
       setInputValue("clock", settings.clock);
       setInputValue("font", settings.font);
-      setInputValue("zero", settings.leadingZero.toString());
+      setInputValue("zero", settings.leadingZero);
       setInputValue("css", settings.customCss);
-      setInputValue("icon", settings.displayIcon.toString());
-      (document.getElementById("activateDebugMode") as HTMLInputElement).checked = settings.activateDebugMode;
+      setInputValue("icon", settings.displayIcon);
+      setInputValue("activateDebugMode", settings.activateDebugMode);
+      colorSchemeOnChange(document.getElementById("colorScheme") as HTMLSelectElement);
     }, error => setStatus(`Error: ${error}`));
+}
+
+function colorSchemeOnChange(elm: HTMLSelectElement) {
+  const value = elm.value as ColorScheme;
+  const enable = (value === "custom");
+  (document.getElementById("background") as HTMLInputElement).disabled = !enable;
+  (document.getElementById("foreground") as HTMLInputElement).disabled = !enable;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -70,6 +99,9 @@ document.addEventListener("DOMContentLoaded", () => {
     document.execCommand("copy");
   };
 
+  document.getElementById("colorScheme")!.addEventListener("change", ({ target }) => {
+    colorSchemeOnChange(target as HTMLSelectElement);
+  })
 });
 
 document.querySelector("form")!.addEventListener("submit", saveOptions);
