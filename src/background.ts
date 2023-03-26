8543f92da92442fc586e75516027a8a8b1b4749a
Jérémie Bertrand <jeremie.bertrand@murena.io>
2023-03-26 09:45:23 +0200

    fix manifest and background script for edge

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,11 @@
-browser.runtime.onInstalled.addListener(function (details) {
+declare var chrome: typeof browser;
+let browserInstance = chrome;
+if (typeof chrome === "undefined" || Object.getPrototypeOf(chrome) !== Object.prototype) {
+    browserInstance = browser;
+}
+
+browserInstance.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {
-        browser.storage.sync.set({ displayOptionsButton: true });
+        browserInstance.storage.sync.set({ displayOptionsButton: true });
     }
 });
